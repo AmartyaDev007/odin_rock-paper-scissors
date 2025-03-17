9155f15c0da5cb7715ec8a6ef5d319ecb81b1d14
AmartyaDev007 <162921312+AmartyaDev007@users.noreply.github.com>
2025-03-17 20:33:17 +0530

    added a function that returns one of three values using math.random

--- code/game.js
+++ code/game.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+
+function getComputerChoice() { 
+    let randomNum = Math.floor(Math.random() * 3);
+
+    if (randomNum === 0) {
+        return "rock";
+    }
+    else if (randomNum === 1) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
+
+let computer_result = getComputerChoice();
+
