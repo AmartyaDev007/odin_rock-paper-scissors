535f00cd080424e7bb132135540cbe91974c301c
AmartyaDev007 <162921312+AmartyaDev007@users.noreply.github.com>
2025-03-17 22:38:29 +0530

    added function playRound that will print the winner and add a point based on the winner

--- code/game.js
+++ code/game.js
@@ -17,8 +17,36 @@ function getHumanChoice() {
     return prompt("rock, paper or scissors?");
 }
 
-let computer_result = getComputerChoice();
-let human_result = getHumanChoice();
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        console.log(`You won! Your choice: ${humanChoice}; Computer choice: ${computerChoice}.`);
+        humanScore += 1;
+    }
+    else if (
+        (humanChoice === "scissors" && computerChoice === "rock") ||
+        (humanChoice === "rock" && computerChoice === "paper") ||
+        (humanChoice === "paper" && computerChoice === "scissors")
+    ) {
+        console.log(`You lost! Your choice: ${humanChoice}; Computer choice: ${computerChoice}.`);
+        computerScore += 1;
+    }
+    else {
+        console.log("It's a tie for the round.");
+    }
+
+
+}
 
 let humanScore = 0;
 let computerScore = 0;
+
+let computerSelection = getComputerChoice();
+let humanSelection = getHumanChoice();
+
+playRound(humanSelection, computerSelection);
