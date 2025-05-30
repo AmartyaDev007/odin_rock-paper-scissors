34ea9bdd06b8bce1a9197fb09c5eec83984d0b7b
AmartyaDev007 <162921312+AmartyaDev007@users.noreply.github.com>
2025-05-30 07:13:43 +0530

    Add js for roundinput and playsection

--- code/game.js
+++ code/game.js
@@ -1,3 +1,148 @@
+let humanScore = 0;
+let computerScore = 0;
+let humanChoice = null;
+let computerChoice = null;
+let roundTracker = 1;
+let round = null;
+
+const promptPlayConfirmBtn = document.querySelector("#play-btn");
+const roundNumInput = document.querySelector("#roundInput")
+
+const promptGameSection = document.querySelector(".prompt-rounds");
+const mainGameSection = document.querySelector(".main-game");
+
+const allPlayerChoices = document.querySelector(".all-buttons");
+const pickYourChoiceTxt = document.querySelector(".choice-text");
+
+const resultDisplay = document.querySelector(".results-display");
+const playAgainPrompt = document.querySelector(".replay-game");
+
+
+const playerScoreSpan = document.querySelector("#player-score");
+const computerScoreSpan = document.querySelector("#computer-score");
+
+
+
+promptPlayConfirmBtn.addEventListener("click", () => {
+    if (isNaN(Number(roundNumInput.value)) || roundNumInput.value > 10 || roundNumInput.value < 1) {
+        alert("Max. allowed rounds is 10.");
+        return;
+    }
+
+    promptGameSection.style.display = 'none';
+    mainGameSection.style.display = 'block';
+    round = parseInt(roundNumInput.value);
+    console.log(round);
+});
+
+
+
+
+
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        humanScore += 1;
+        playerScoreSpan.textContent = humanScore;
+    }
+    else if (
+        (humanChoice === "scissors" && computerChoice === "rock") ||
+        (humanChoice === "rock" && computerChoice === "paper") ||
+        (humanChoice === "paper" && computerChoice === "scissors")
+    ) {
+        computerScore += 1;
+        computerScoreSpan.textContent = computerScore;
+    }
+    else {
+        console.log("It's a tie for the round.");
+    }
+    console.log(roundTracker);
+}
+
+
+function getComputerChoice() { 
+    let randomNum = Math.floor(Math.random() * 3);
+
+    if (randomNum === 0) {
+        return "rock";
+    }
+    else if (randomNum === 1) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
+
+
+function checkRoundOverflow() { 
+    if (roundTracker >= round) {
+        allPlayerChoices.style.display = 'none';
+        pickYourChoiceTxt.style.display = 'none';
+
+        resultDisplay.style.display = 'block';
+        playAgainPrompt.style.display = 'block';
+        return true;
+    }
+    return false;
+}
+
+
+function getPlayerChoice(target) {
+
+    switch(target.id) {
+        
+        case 'rock':
+            humanChoice = "rock";
+            roundTracker += 1;
+            break;
+
+        case 'paper':
+            humanChoice = "paper";
+            roundTracker += 1;
+            break;
+
+        case 'scissors':
+            humanChoice = "scissors";
+            roundTracker += 1;
+            break;
+    }
+    playRound(humanChoice, computerChoice);
+}
+
+
+allPlayerChoices.addEventListener('click', (event) => {
+    
+    let target = event.target.closest('button');
+
+    computerChoice = getComputerChoice();
+
+    if (checkRoundOverflow() == false) {
+        getPlayerChoice(target);
+    }
+
+})
+
+// here is where you will add result reset :) 
+
+// add divs for , current round , total rounds and result of the current round : ) 
+
+
+
+
+
+
+
+
+
+
+/*
 
 function getComputerChoice() { 
     let randomNum = Math.floor(Math.random() * 3);
@@ -50,11 +195,13 @@ function playGame() {
     playRound(humanSelection, computerSelection);
 }
 
+// Here, the main starts
+
 let humanScore = 0;
 let computerScore = 0;
 
 
-// for loop that calls playgame 5 times
+// REMOVED THE LOOP FOR DOM (TODO: REMOVE THIS COMMENT)
 for (let n = 0; n < 5; n++) {
     playGame()
 }
@@ -74,3 +221,5 @@ else {
         Your score: ${humanScore}
         Computer's score: ${computerScore}`)
 }
+
+*/
